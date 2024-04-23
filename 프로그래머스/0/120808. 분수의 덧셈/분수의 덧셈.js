ba6f6b467049df20ec1b7e561123a8c180b2f15b
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-04-23 10:10:49 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.js
@@ -1,6 +1,7 @@
 function solution(numer1, denom1, numer2, denom2) {
     let numer = numer1 * denom2 + numer2 * denom1;
-let denom = denom1 * denom2;
+    let denom = denom1 * denom2;
+    
     const getGcd = (a, b) => (a % b == 0 ? b : getGcd(b, a % b));
     let gcd = getGcd(numer, denom);
     
