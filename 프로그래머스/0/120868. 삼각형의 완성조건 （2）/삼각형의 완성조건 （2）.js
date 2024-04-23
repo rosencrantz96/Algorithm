e84a81cde5f7a729ebc8f0f3283438c70f44f5ff
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-04-23 17:56:39 +0900

    [level 0] Title: 삼각형의 완성조건 (2), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
+++ 프로그래머스/0/120868. 삼각형의 완성조건 （2）/삼각형의 완성조건 （2）.js
@@ -3,11 +3,15 @@ function solution(sides) {
     let short = Math.min(sides[0], sides[1]);
     let possible = [];
 
-    for (let i = long - short + 1; i <= long; i++) {
-        possible.push(i);
-    }
-    for (let i = long + 1; i < long + short; i++) {
-        possible.push(i);
+    // for (let i = long - short + 1; i <= long; i++) {
+    //     possible.push(i);
+    // }
+    // for (let i = long + 1; i < long + short; i++) {
+    //     possible.push(i);
+    // }
+    
+    for (let i = long-short+1; i< long+short; i++) {
+        possible.push(i)
     }
     return possible.length;
 }
