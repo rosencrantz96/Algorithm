8eb049f7f70864bc93e0e7c04f79a6b1c211bf37
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-04-25 10:41:16 +0900

    [level 0] Title: n의 배수 고르기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.js
@@ -0,0 +1,7 @@
+function solution(n, numlist) {
+    let answer = [];
+    for (let i = 0; i < numlist.length; i++) {
+        if (numlist[i] % n === 0) answer.push(numlist[i]);
+    }
+    return answer;
+}
