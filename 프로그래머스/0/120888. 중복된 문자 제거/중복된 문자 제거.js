e2f046fd5c225643fa61ba19813446fd09e9cf89
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-04-30 09:21:20 +0900

    [level 0] Title: 중복된 문자 제거, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.js
@@ -0,0 +1,10 @@
+function solution(my_string) {
+    let arr = my_string.split('');
+    let newArr = [];
+    
+    for (let i=0; i<arr.length; i++) {
+        if(!newArr.includes(arr[i])) newArr.push(arr[i])
+    }
+    
+    return newArr.join().replaceAll(',', '');
+}
