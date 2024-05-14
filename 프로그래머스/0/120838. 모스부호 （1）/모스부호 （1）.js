48e340ba421940e31a3c90da3611fde2cce3774c
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-05-14 10:35:07 +0900

    [level 0] Title: 모스부호 (1), Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,18 @@
+function solution(letter) {
+    const morse = { 
+        '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+        '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+        '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+        '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+        '-.--':'y','--..':'z'
+    };
+    
+    let code = letter.split(' ');
+    let decode = [];
+    
+    for (let i=0; i<code.length; i++) {
+        decode.push(morse[code[i]]);
+    }
+    
+    return decode.join('');
+}
