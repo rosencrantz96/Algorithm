0f59c4625c6455c82effecc3b9e6248c9573c4cb
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-05-21 17:17:28 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,12 @@
+function solution(a, b) {
+    const calc1 = (a, b) => {
+        return a.toString() + b.toString();
+    }
+    const calc2 = (a, b) => {
+        return b.toString() + a.toString();
+    }
+    let result1 = calc1(a, b);
+    let result2 = calc2(a, b);
+    
+    return result1 >= result2 ? +result1 : +result2
+}
