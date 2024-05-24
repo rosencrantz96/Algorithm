e94b4d89b000495f30974ce920172fed48e555f9
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-05-24 10:11:23 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,18 +1,15 @@
 function solution(n) {
-    const calc = (n) => {
-        let answer = 0;
-        if (n % 2 != 0) {
-            for (let i=1; i<=n; i += 2) {
-                console.log(i)
-                answer += i;
-            }
-            return answer;
-        } else {
-            for (let i=0; i<=n; i += 2) {
-                answer += Math.pow(i, 2)
-            }
-            return answer;
+    let answer = 0;
+    if (n % 2 != 0) {
+        for (let i=1; i<=n; i += 2) {
+            answer += i;
         }
+        return answer;
+    } else {
+        for (let i=0; i<=n; i += 2) {
+            answer += Math.pow(i, 2)
+        }
+        return answer;
     }
-    return calc(n);
+    return answer;
 }
