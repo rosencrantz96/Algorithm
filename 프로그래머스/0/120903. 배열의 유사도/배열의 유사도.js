db673b93d7b0eb3456a241276dea794ca09a5daa
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-06-03 15:19:41 +0900

    [level 0] Title: 배열의 유사도, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
+++ 프로그래머스/0/120903. 배열의 유사도/배열의 유사도.js
@@ -1,6 +1,6 @@
 function solution(s1, s2) {
     let answer = 0;
-    let result = s2.forEach((el) => {
+    s2.forEach((el) => {
         for(v of s1) {
             if (el === v) answer++;
         }
