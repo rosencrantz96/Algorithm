41230ed981cec2e8a848c023f32987ab92c9ae0e
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-06-05 14:11:34 +0900

    [level 0] Title: 특별한 이차원 배열 2, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181831. 특별한 이차원 배열 2/특별한 이차원 배열 2.js
@@ -0,0 +1,10 @@
+function solution(arr) {
+    let answer = 0;
+    for (let i=0; i<arr.length; i++) {
+        for (let j=0; j<arr[0].length; j++) {
+            if (arr[i][j] == arr[j][i]) continue;
+            else return 0;
+        }
+    }
+    return 1
+}
