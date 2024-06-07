ed3fd28d6bf5ef8716c194dd433a65e6775fa278
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-06-07 11:38:50 +0900

    [level 0] Title: 배열 만들기 5, Time: 1.03 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
+++ 프로그래머스/0/181912. 배열 만들기 5/배열 만들기 5.js
@@ -1,3 +1,3 @@
 function solution(intStrs, k, s, l) {
-    return intStrs.map((el) => +el.slice(s, s+l)).filter(v => Number(v) > k)
+    return intStrs.map((el) => +el.slice(s, s+l)).filter(v => v > k)
 }
