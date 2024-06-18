cded77e2b53d4151b6adc042971b061823e84c71
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-06-18 14:53:56 +0900

    [level 0] Title: k의 개수, Time: 10.48 ms, Memory: 38.7 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.js
+++ 프로그래머스/0/120887. k의 개수/k의 개수.js
@@ -2,9 +2,7 @@ function solution(i, j, k) {
     let str = k.toString();
     return Array.from({ length: j - i + 1 }, (_, idx) => i + idx)
         .map((v) => v.toString())
-        .filter(v => v.includes(str))
         .reduce((a, c) => {
-            c = c.split('');
             for(let char of c) {
                 if (char === str) a++
             }
