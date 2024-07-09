98f668179e8cf8289f01d4800050011dc4170208
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-07-09 21:57:44 +0900

    [level 0] Title: 평행, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120875. 평행/평행.js
@@ -0,0 +1,26 @@
+const isParallel = (dot) => {
+    let [dot1, dot2, dot3, dot4] = dot;
+    // dot1과 dot2를 이은 선분의 기울기
+    let line1 = Math.abs(dot2[1]-dot1[1]) / Math.abs(dot2[0] - dot1[0]);
+    // dot3과 dot4를 이은 선분의 기울기
+    let line2 = Math.abs(dot4[1]-dot3[1]) / Math.abs(dot4[0]-dot3[0]);
+
+    return line1 === line2 ? true : false;
+}
+
+function solution(dots) {
+    // x좌표를 기준으로 배열을 정렬하기 
+    let x = dots.sort((a, b) => {
+        a = a[0];
+        b = b[0];
+        return a - b;
+    })
+    // y좌표를 기준으로 배열을 정렬하기 
+    let y = dots.sort((a, b) => {
+        a = a[1];
+        b = b[1];
+        return a - b;
+    })
+
+    return isParallel(x) ? 1 : isParallel(y) ? 1 : 0;
+}
