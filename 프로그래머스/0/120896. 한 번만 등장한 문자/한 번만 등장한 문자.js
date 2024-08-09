ac2316ad7e1dcae53016437e2ff884ccacac7177
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-08-09 13:54:21 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.17 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,17 +1,13 @@
 function solution(s) {
-    // s에 몇 번 등장하는지 세어주는 함수
-    let counting = (str) => {
-        let result = 0;
-        for(let i=0; i<s.length; i++) {
-            if (s[i] == str) result++;
-        }
-        return result;
-    }
+    /*
+    1. 원본 문자열을 Set을 사용해서 중복되지 않는 요소만 배열로 추출한다.
+    2. 배열을 순회하며 각 요소로 원본 문자열을 잘랐을 때 배열의 크기가 2가 되는 녀석만 고른다. (2면 하나만 존재하는 거니깐, map, filter 사용)
+    3. 사전순으로 정렬한 후 문자열로 붙여서 반환한다. (sort, join 사용)
+    */
     
-    let rep = Array.from(new Set(s));
-    let repCnt = rep.map((v) => { return v = counting(v) });
-
-    let result = rep.filter((v, i) => repCnt[i] ==1).sort().join('')
+    let charArr = Array.from(new Set(s));
     
-    return result; 
-} 
+    return charArr.map((v) => { 
+            if (s.split(v).length === 2) return v 
+        }).filter(v => v).sort().join('');
+}
