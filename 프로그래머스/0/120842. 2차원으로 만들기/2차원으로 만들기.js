5d3a2b94cafccc9670174144a5a07ffcf5c08c71
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-08-09 14:14:15 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.js
@@ -1,7 +1,14 @@
 function solution(num_list, n) {
+    /*
+    1. 정답을 담을 빈 배열 선언
+    2. num_list를 순회하면서 n개씩 잘라낸 배열을 정답 배열에 넣어주기
+    */
+    
     let answer = [];
-    for (let i = 0; i < num_list.length; i += n) {
-        answer.push(num_list.slice(i, i + n));
+    
+    for (let i=0; i<num_list.length; i+=n) {
+        answer.push(num_list.slice(i, i+n))
     }
+    
     return answer;
 }
