2497fe71ca390780784d2c322d398044ea1fe7f6
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-09-05 23:34:47 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -2,9 +2,6 @@ function solution(s) {
     let length = s.length;
     let middleIndex = Math.floor(length/2);
     
-    if (!(length % 2)) {
-        return s[middleIndex - 1] + s[middleIndex];
-    } else {
-       return s[middleIndex]; 
-    }
+    if (!(length % 2)) return s[middleIndex - 1] + s[middleIndex];
+    else return s[middleIndex]; 
 }
