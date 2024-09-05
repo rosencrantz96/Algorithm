7957175f36d46c98e18ff61ba21f2b34ca085b28
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-09-05 23:33:49 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -1,8 +1,6 @@
 function solution(phone_number) {
     return [...phone_number].map((v, i) => {
-        if (i < phone_number.length - 4) {
-            return v = '*'
-        }
+        if (i < phone_number.length - 4) return v = '*'
         return v;
     }).join('');
 }
