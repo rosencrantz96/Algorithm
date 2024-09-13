1bb0895f3e136ff0e01c93b24c2b2223cab3e3b1
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-09-13 09:56:10 +0900

    [level 1] Title: 약수의 합, Time: 0.61 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.js
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.js
@@ -1,8 +1,12 @@
 function solution(n) {
+    // 정수 배열 만들기
     let intArr = Array.from({length: n}, (_, i) => i + 1);
     
+    // 약수 합 
+    // 아~~ 왜 익스텐션 안되는데~~
     return intArr.reduce((a, c, i) => {
         if (n%c === 0) return a+=c;
         return a;
     }, 0);
+    
 }
