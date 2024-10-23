8c2158e45cb2449baff4795f47b4ebc1aa28d5c2
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2024-10-23 12:54:05 +0900

    [level 0] Title: 문자열 정수의 합, Time: 0.07 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
+++ 프로그래머스/0/181849. 문자열 정수의 합/문자열 정수의 합.py
@@ -1,12 +1,4 @@
-def solution(num_str):
-    nums = []
-    sum = 0
-    
-    for i in range(len(num_str)):
-        nums.append(int(num_str[i]))
-        
-    for i in range(len(nums)):
-        sum += nums[i]
+from functools import reduce
 
-    return sum
-    
+def solution(num_str):
+    return reduce(lambda x, y: int(x) + int(y), num_str)
