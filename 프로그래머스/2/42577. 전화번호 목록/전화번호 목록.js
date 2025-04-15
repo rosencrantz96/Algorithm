f3c196b446705088acecabb44824c43fc8ebc65b
KWON Sookyoung <115062680+rosencrantz96@users.noreply.github.com>
2025-04-15 11:15:09 +0900

    [level 2] Title: 전화번호 목록, Time: 131.21 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.js
@@ -4,5 +4,6 @@ function solution(phone_book) {
     for (let i=0; i<phone_book.length -1; i++) {
         if (phone_book[i+1].startsWith(phone_book[i])) return false;
     }
+    
     return true;
 }
